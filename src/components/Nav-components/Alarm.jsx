a7ed52d7b65bb6d9708bae97cd28dc2d8591e1af
황주혁 <hjh041123@naver.com>
2024-11-24 07:02:04 +0900

    last

--- src/components/Nav-components/Alarm.jsx
+++ src/components/Nav-components/Alarm.jsx
@@ -2,43 +2,72 @@ import React from "react";
 import axios from "axios";
 import alarm from "../../assets/images/nav/alarm_img.png";
 import arrowLeft from "../../assets/images/nav/arrow_left(black).png";
-import "../../assets/css/nav/Alarm.css";
+import "../../assets/css/nav/Alarm.css";import { initializeApp } from "firebase/app";
+import { getToken } from "firebase/messaging";
+import { getMessaging, onMessage } from "firebase/messaging";
+import { registerServiceWorker } from "./registerServiceWorker.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAAnZaALoGwQ2XEZcYVoJu99d_9JHbfftw",
+    authDomain: "goorm-2dae7.firebaseapp.com",
+    projectId: "goorm-2dae7",
+    storageBucket: "goorm-2dae7.firebasestorage.app",
+    messagingSenderId: "246219988600",
+    appId: "1:246219988600:web:36151278b487958c79178d",
+    measurementId: "G-SS3P0PGPWF"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const messaging = getMessaging(app);
+console.log("기초 설정 완료");
+
+
+
+export async function handleAllowNotification() {
+    registerServiceWorker();
+    try {
+        const permission = await Notification.requestPermission();
+
+        if (permission === "granted") {
+            const token = await getToken(messaging, {
+                vapidKey: "BHXRXqhiFaBr2OZC4qTQz7t_qGJc6m6NuxtWku4Xpw_fCZYvz_uiNVq2dlDzEP7VMHsjaxPJmCIPduCfBgaxemk"
+            });
+            if (token) {
+                console.log("토큰", token);
+                console.log("http://3.93.236.79:8080/notifications/add-token?token="+token+"&type=web");
+                axios.get("http://3.93.236.79:8080/notifications/add-token?token="+token+"&type=web", {withCredentials:false});
+            } else {
+                alert(
+                    "토큰 등록이 불가능 합니다. 생성하려면 권한을 허용해주세요"
+                );
+            }
+        } else if (permission === "denied") {
+            alert(
+                "알림 권한이 차단되었습니다. 알림을 사용하시려면 권한을 허용해주세요"
+            );
+        }
+    } catch (error) {
+        console.error("푸시 토큰 가져오는 중에 에러 발생", error);
+    }
+}
+
+onMessage(messaging, (payload) => {
+    // console.log("알림 도착 ", payload);
+    const notificationTitle = payload.notification.title;
+    const notificationOptions = {
+        body: payload.notification.body
+    };
+
+    if (Notification.permission === "granted") {
+        new Notification(notificationTitle, notificationOptions);
+    }
+});
+
+
 
 const Alarm = () => {
-  const handlePushNotification = async () => {
-    try {
-      const accessToken = localStorage.getItem("access_token"); // 로컬 스토리지에서 액세스 토큰 가져오기
-
-      if (!accessToken) {
-        alert("로그인이 필요합니다.");
-        return;
-      }
-
-      // 사용자의 푸시 토큰을 등록하기 위한 요청
-      const registrationResponse = await axios.post(
-        "https://kapi.kakao.com/v2/push/register",
-        {
-          uuid: "사용자_고유_아이디", // 사용자 고유의 UUID를 사용
-          push_type: "gcm", // 구글 클라우드 메시징 사용
-          push_token: "사용자_푸시_토큰", // 사용자의 디바이스에서 받아온 푸시 토큰
-        },
-        {
-          headers: {
-            Authorization: `Bearer ${accessToken}`,
-            "Content-Type": "application/json",
-          },
-        }
-      );
-
-      if (registrationResponse.status === 200) {
-        // 성공적으로 푸시 토큰이 등록되었을 때
-        alert("카카오톡 푸시 알림이 설정되었습니다.");
-      }
-    } catch (error) {
-      console.error("푸시 알림 설정 중 오류 발생:", error);
-      alert("푸시 알림 설정 중 오류가 발생했습니다.");
-    }
-  };
+  
 
   const handleBackClick = () => {
     window.history.back();
@@ -52,7 +81,7 @@ const Alarm = () => {
         <p>돌아가기</p>
       </div>
         <img src={alarm} alt="alarm" className="alarm-image" />
-        <button className="alarm-btn" onClick={handlePushNotification}>
+        <button className="alarm-btn" onClick={handleAllowNotification}>
           카카오톡으로 알림받기
         </button>
         <div className="alarm-info">•  앱으로 들어가지 않아도 카카오톡으로</div>
